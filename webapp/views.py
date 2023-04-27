e710fb2896284809656729a408fb0255599acc83
Jesse-L2 <jesse.little@utexas.edu>
2023-04-27 14:15:51 -0500

    Add view individual record

--- webapp/views.py
+++ webapp/views.py
@@ -47,3 +47,11 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'record': record})
+    else:
+        messages.success(request, "You must be logged in to view that page.")
+        return redirect('home')
