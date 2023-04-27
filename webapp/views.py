889901461704096770f81293270180c1e674422d
Jesse-L2 <jesse.little@utexas.edu>
2023-04-27 14:46:26 -0500

    Add update record

--- webapp/views.py
+++ webapp/views.py
@@ -79,5 +79,19 @@ def add_record(request):
                 return redirect('home')
         return render(request, 'add_record.html', {'form': form})
     else:
-        messages.success(request, "You Must Be Logged In...")
+        messages.success(request, "You must be logged in to perform that function.")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been successfully updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must be logged in to perform that function.")
         return redirect('home')
